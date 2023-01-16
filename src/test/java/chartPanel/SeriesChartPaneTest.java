d5d05e0f2b9ab805e659f7f1fb69e0182f909020
TianyuWei-IC <107886797+TianyuWei-IC@users.noreply.github.com>
2023-01-16 05:49:28 +0000

    Update SeriesChartPaneTest.java

--- src/test/java/chartPanel/SeriesChartPaneTest.java
+++ src/test/java/chartPanel/SeriesChartPaneTest.java
@@ -29,7 +29,7 @@ class SeriesChartPaneTest {
 
             // create a patient
             GUI_test mainGUI = new GUI_test();
-            Patient current_patient = new Patient("Person","current","abnormal_patient","male",2002,35.0,38.0,50,110,85,145,55,90,12,20, mainGUI);
+            Patient current_patient = new Patient("Person","current","normal_2","male",2002,35.0,38.0,50,110,85,145,55,90,12,20, mainGUI);
 
             SeriesChartPane chartPane = new SeriesChartPane(input,time,"ecg1", current_patient,"title","real time");
             assertEquals(input,chartPane.getDataInput());
